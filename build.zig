4f15c4bb0285a1bd0dc2338469ce4a33fc6ea354
Alexandre Chêne <kooparse@protonmail.com>
2020-11-10 22:14:50 +0100

    Better build file

--- build.zig
+++ build.zig
@@ -4,15 +4,9 @@ const Builder = std.build.Builder;
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
-    var lib = b.addStaticLibrary("zalgebra", "src/main.zig");
-    lib.setBuildMode(mode);
-
     var tests = b.addTest("src/main.zig");
     tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
-
-    b.default_step.dependOn(&lib.step);
-    lib.install();
 }
