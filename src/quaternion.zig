dbdadbecebeb9fda94dc2930bf6d99c451dab219
Alexandre Chêne <kooparse@users.noreply.github.com>
2022-01-06 15:02:35 +0100

    Revert "New generic vectors, using std.meta.Vector"

--- src/quaternion.zig
+++ src/quaternion.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
 const root = @import("main.zig");
-const meta = std.meta;
-const generic_vector = @import("generic_vector.zig");
+const vec4 = @import("vec4.zig");
+const vec3 = @import("vec3.zig");
 const mat4 = @import("mat4.zig");
 const math = std.math;
 const expectEqual = std.testing.expectEqual;
 const expect = std.testing.expect;
 const assert = std.debug.assert;
 
-const GenericVector = generic_vector.GenericVector;
-
-const Vec3 = generic_vector.Vec3;
-const Vec4 = generic_vector.Vec4;
+const Vec3 = vec3.Vec3;
+const Vector3 = vec3.Vector3;
+const Vec4 = vec4.Vec4;
+const Vector4 = vec4.Vector4;
 const Mat4x4 = mat4.Mat4x4;
 
 pub const Quat = Quaternion(f32);
@@ -22,8 +22,6 @@ pub fn Quaternion(comptime T: type) type {
     if (@typeInfo(T) != .Float) {
         @compileError("Quaternion not implemented for " ++ @typeName(T));
     }
-
-    const Vector3 = meta.Vector(3, T);
 
     return extern struct {
         w: T,
@@ -43,7 +41,7 @@ pub fn Quaternion(comptime T: type) type {
             };
         }
 
-        /// Shorthand for (1, 0, 0, 0).
+        /// Construct most basic quaternion.
         pub fn zero() Self {
             return Self.new(1, 0, 0, 0);
         }
@@ -54,17 +52,15 @@ pub fn Quaternion(comptime T: type) type {
             return Self.new(slice[3], slice[0], slice[1], slice[2]);
         }
 
-        // Construct new quaternion from given `W` component and Vector3.
-        pub fn fromVec3(w: T, axis: Vector3) Self {
+        pub fn fromVec3(w: T, axis: Vector3(T)) Self {
             return .{
                 .w = w,
-                .x = axis[0],
-                .y = axis[1],
-                .z = axis[2],
+                .x = axis.x,
+                .y = axis.y,
+                .z = axis.z,
             };
         }
 
-        /// Return true if two quaternions are equal.
         pub fn eql(left: Self, right: Self) bool {
             return (left.w == right.w and
                 left.x == right.x and
@@ -72,7 +68,6 @@ pub fn Quaternion(comptime T: type) type {
                 left.z == right.z);
         }
 
-        /// Construct new normalized quaternion from a given one.
         pub fn norm(self: Self) Self {
             const l = length(self);
             assert(l != 0);
@@ -85,12 +80,10 @@ pub fn Quaternion(comptime T: type) type {
             );
         }
 
-        /// Return the length (magnitude) of quaternion.
         pub fn length(self: Self) T {
             return @sqrt(self.dot(self));
         }
 
-        /// Substraction between two quaternions.
         pub fn sub(left: Self, right: Self) Self {
             return Self.new(
                 left.w - right.w,
@@ -100,7 +93,6 @@ pub fn Quaternion(comptime T: type) type {
             );
         }
 
-        /// Addition between two quaternions.
         pub fn add(left: Self, right: Self) Self {
             return Self.new(
                 left.w + right.w,
@@ -110,8 +102,6 @@ pub fn Quaternion(comptime T: type) type {
             );
         }
 
-        /// Quaternions' multiplication.
-        /// Produce a new quaternion from given two quaternions.
         pub fn mult(left: Self, right: Self) Self {
             var q: Self = undefined;
 
@@ -123,7 +113,6 @@ pub fn Quaternion(comptime T: type) type {
             return q;
         }
 
-        /// Multiply each component by the given scalar.
         pub fn scale(mat: Self, scalar: T) Self {
             var result: Self = undefined;
             result.w = mat.w * scalar;
@@ -132,11 +121,6 @@ pub fn Quaternion(comptime T: type) type {
             result.z = mat.z * scalar;
 
             return result;
-        }
-
-        /// Negate the given quaternion
-        pub fn negate(self: Self) Self {
-            return self.scale(-1);
         }
 
         /// Return the dot product between two quaternion.
@@ -227,31 +211,31 @@ pub fn Quaternion(comptime T: type) type {
                 }
             }
 
-            return Self.scale(result, 0.5 / @sqrt(t));
+            return Self.scale(result, 0.5 / math.sqrt(t));
         }
 
         /// Convert all Euler angles to quaternion.
-        pub fn fromEulerAngle(axis: Vector3) Self {
-            const x = Self.fromAxis(axis[0], GenericVector(3, T).right());
-            const y = Self.fromAxis(axis[1], GenericVector(3, T).up());
-            const z = Self.fromAxis(axis[2], GenericVector(3, T).forward());
+        pub fn fromEulerAngle(axis: Vector3(T)) Self {
+            const x = Self.fromAxis(axis.x, Vec3.right());
+            const y = Self.fromAxis(axis.y, Vec3.up());
+            const z = Self.fromAxis(axis.z, Vec3.forward());
 
             return z.mult(y.mult(x));
         }
 
         /// Convert Euler angle around specified axis to quaternion.
-        pub fn fromAxis(degrees: T, axis: Vector3) Self {
+        pub fn fromAxis(degrees: T, axis: Vector3(T)) Self {
             const radians = root.toRadians(degrees);
 
             const rot_sin = @sin(radians / 2.0);
-            const quat_axis = GenericVector(3, T).norm(axis) * @splat(3, rot_sin);
+            const quat_axis = axis.norm().scale(rot_sin);
             const w = @cos(radians / 2.0);
 
             return Self.fromVec3(w, quat_axis);
         }
 
         /// Extract euler angles from quaternion.
-        pub fn extractRotation(self: Self) Vector3 {
+        pub fn extractRotation(self: Self) Vector3(T) {
             const yaw = math.atan2(
                 T,
                 2.0 * (self.y * self.z + self.w * self.x),
@@ -266,10 +250,10 @@ pub fn Quaternion(comptime T: type) type {
                 self.w * self.w + self.x * self.x - self.y * self.y - self.z * self.z,
             );
 
-            return [3]T{ root.toDegrees(yaw), root.toDegrees(pitch), root.toDegrees(roll) };
+            return Vector3(T).new(root.toDegrees(yaw), root.toDegrees(pitch), root.toDegrees(roll));
         }
 
-        /// Linear interpolation between two quaternions.
+        /// Lerp between two quaternions.
         pub fn lerp(left: Self, right: Self, t: f32) Self {
             const w = root.lerp(T, left.w, right.w, t);
             const x = root.lerp(T, left.x, right.x, t);
@@ -289,7 +273,7 @@ pub fn Quaternion(comptime T: type) type {
             // We need the absolute value of the dot product to take the shortest path
             if (cos_theta < 0.0) {
                 cos_theta *= -1;
-                right1 = right.negate();
+                right1 = right.scale(-1);
             }
 
             if (cos_theta > ParallelThreshold) {
@@ -303,14 +287,14 @@ pub fn Quaternion(comptime T: type) type {
             }
         }
 
-        /// Rotate the Vector3 v using the sandwich product.
+        /// Rotate the vector v using the sandwich product.
         /// Taken from "Foundations of Game Engine Development Vol. 1 Mathematics".
-        pub fn rotateVec(self: Self, v: Vector3) Vector3 {
+        pub fn rotateVec(self: Self, v: Vector3(T)) Vector3(T) {
             const q = self.norm();
-            const b: Vector3 = [3]T{ q.x, q.y, q.z };
-            const b2 = b[0] * b[0] + b[1] * b[1] + b[2] * b[2];
+            const b = Vector3(T).new(q.x, q.y, q.z);
+            const b2 = b.x * b.x + b.y * b.y + b.z * b.z;
 
-            return (v * @splat(3, q.w * q.w - b2)) + (b * @splat(3, GenericVector(3, T).dot(v, b) * 2.0)) + (GenericVector(3, T).cross(b, v) * @splat(3, q.w * 2.0));
+            return v.scale(q.w * q.w - b2).add(b.scale(v.dot(b) * 2.0)).add(b.cross(v).scale(q.w * 2.0));
         }
     };
 }
@@ -330,7 +314,7 @@ test "zalgebra.Quaternion.fromSlice" {
 }
 
 test "zalgebra.Quaternion.fromVec3" {
-    const q = Quat.fromVec3(1.5, [3]f32{ 2.6, 3.7, 4.7 });
+    const q = Quat.fromVec3(1.5, Vec3.new(2.6, 3.7, 4.7));
 
     try expectEqual(q.w, 1.5);
     try expectEqual(q.x, 2.6);
@@ -339,62 +323,62 @@ test "zalgebra.Quaternion.fromVec3" {
 }
 
 test "zalgebra.Quaternion.fromVec3" {
-    const q1 = Quat.fromVec3(1.5, [3]f32{ 2.6, 3.7, 4.7 });
-    const q2 = Quat.fromVec3(1.5, [3]f32{ 2.6, 3.7, 4.7 });
-    const q3 = Quat.fromVec3(1, [3]f32{ 2.6, 3.7, 4.7 });
+    const q1 = Quat.fromVec3(1.5, Vec3.new(2.6, 3.7, 4.7));
+    const q2 = Quat.fromVec3(1.5, Vec3.new(2.6, 3.7, 4.7));
+    const q3 = Quat.fromVec3(1, Vec3.new(2.6, 3.7, 4.7));
 
     try expectEqual(q1.eql(q2), true);
     try expectEqual(q1.eql(q3), false);
 }
 
 test "zalgebra.Quaternion.norm" {
-    const q1 = Quat.fromVec3(1, [3]f32{ 2, 2.0, 2.0 });
-    const q2 = Quat.fromVec3(0.2773500978946686, [3]f32{ 0.5547001957893372, 0.5547001957893372, 0.5547001957893372 });
+    const q1 = Quat.fromVec3(1, Vec3.new(2, 2.0, 2.0));
+    const q2 = Quat.fromVec3(0.2773500978946686, Vec3.new(0.5547001957893372, 0.5547001957893372, 0.5547001957893372));
 
     try expectEqual(q1.norm().eql(q2), true);
 }
 
 test "zalgebra.Quaternion.fromEulerAngle" {
-    const q1 = Quat.fromEulerAngle([3]f32{ 10, 5, 45 });
-    const res_q1 = q1.extractRotation();
+    const q1 = Quat.fromEulerAngle(Vec3.new(10, 5, 45));
+    const rot1 = q1.extractRotation();
 
-    const q2 = Quat.fromEulerAngle([3]f32{ 0, 55, 22 });
-    const res_q2 = q2.toMat4().extractRotation();
+    const q2 = Quat.fromEulerAngle(Vec3.new(0, 55, 22));
+    const rot2 = q2.toMat4().extractRotation();
 
-    try expectEqual(Vec3.eql(res_q1, [3]f32{ 9.999999046325684, 5.000000476837158, 45 }), true);
-    try expectEqual(Vec3.eql(res_q2, [3]f32{ 0, 47.2450294, 22 }), true);
+    try expectEqual(Vec3.eql(rot1, Vec3.new(9.999999046325684, 5.000000476837158, 45)), true);
+    try expectEqual(Vec3.eql(rot2, Vec3.new(0, 47.2450294, 22)), true);
 }
 
 test "zalgebra.Quaternion.fromAxis" {
-    const q1 = Quat.fromAxis(45, Vec3.up());
+    const q1 = Quat.fromAxis(45, Vec3.new(0, 1, 0));
     const res_q1 = q1.extractRotation();
 
-    try expectEqual(Vec3.eql(res_q1, [3]f32{ 0, 45.0000076, 0 }), true);
+    try expectEqual(Vec3.eql(res_q1, Vec3.new(0, 45.0000076, 0)), true);
 }
 
 test "zalgebra.Quaternion.extractRotation" {
-    const q1 = Quat.fromVec3(0.5, [3]f32{ 0.5, 1, 0.3 });
+    const q1 = Quat.fromVec3(0.5, Vec3.new(0.5, 1, 0.3));
     const res_q1 = q1.extractRotation();
 
-    try expectEqual(Vec3.eql(res_q1, [3]f32{ 129.6000213623047, 44.427005767822266, 114.4107360839843 }), true);
+    try expectEqual(Vec3.eql(res_q1, Vec3.new(129.6000213623047, 44.427005767822266, 114.41073608398438)), true);
 }
 
 test "zalgebra.Quaternion.rotateVec" {
     const eps_value = comptime std.math.epsilon(f32);
-    const q = Quat.fromEulerAngle(Vec3.set(45));
+    const q = Quat.fromEulerAngle(Vec3.new(45, 45, 45));
     const m = q.toMat4();
 
-    const v = Vec3.up();
+    const v = Vec3.new(0, 1, 0);
     const v1 = q.rotateVec(v);
-    const v2 = m.multByVec4([4]f32{ v[0], v[1], v[2], 1.0 });
+    const v2 = m.multByVec4(Vec4.new(v.x, v.y, v.z, 1.0));
 
-    try expect(std.math.approxEqAbs(f32, v1[0], -1.46446585e-01, eps_value));
-    try expect(std.math.approxEqAbs(f32, v1[1], 8.53553473e-01, eps_value));
-    try expect(std.math.approxEqAbs(f32, v1[2], 0.5, eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.x, -1.46446585e-01, eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.y, 8.53553473e-01, eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.z, 0.5, eps_value));
 
-    try expect(std.math.approxEqAbs(f32, v1[0], v2[0], eps_value));
-    try expect(std.math.approxEqAbs(f32, v1[1], v2[1], eps_value));
-    try expect(std.math.approxEqAbs(f32, v1[2], v2[2], eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.x, v2.x, eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.y, v2.y, eps_value));
+    try expect(std.math.approxEqAbs(f32, v1.z, v2.z, eps_value));
 }
 
 test "zalgebra.Quaternion.lerp" {
---
+++ src/vec4.zig
@@ -0,0 +1,353 @@
+const std = @import("std");
+const root = @import("main.zig");
+const math = std.math;
+const panic = std.debug.panic;
+const expectEqual = std.testing.expectEqual;
+
+pub const Vec4 = Vector4(f32);
+pub const Vec4_f64 = Vector4(f64);
+pub const Vec4_i32 = Vector4(i32);
+pub const Vec4_usize = Vector4(usize);
+
+/// A 4 dimensional vector.
+pub fn Vector4(comptime T: type) type {
+    if (@typeInfo(T) != .Float and @typeInfo(T) != .Int) {
+        @compileError("Vector4 not implemented for " ++ @typeName(T));
+    }
+
+    return extern struct {
+        x: T,
+        y: T,
+        z: T,
+        w: T,
+
+        const Self = @This();
+
+        /// Constract vector from given 3 components.
+        pub fn new(x: T, y: T, z: T, w: T) Self {
+            return Self{
+                .x = x,
+                .y = y,
+                .z = z,
+                .w = w,
+            };
+        }
+
+        /// Set all components to the same given value.
+        pub fn set(val: T) Self {
+            return Self.new(val, val, val, val);
+        }
+
+        /// Shorthand for writing vec4.new(0, 0, 0, 0).
+        pub fn zero() Self {
+            return Self.set(0);
+        }
+
+        /// Shorthand for writing vec4.new(1, 1, 1, 1).
+        pub fn one() Self {
+            return Self.set(1);
+        }
+
+        /// Negate the given vector.
+        pub fn negate(self: Self) Self {
+            return self.scale(-1);
+        }
+
+        /// Cast a type to another type. Only for integers and floats.
+        /// It's like builtins: @intCast, @floatCast, @intToFloat, @floatToInt
+        pub fn cast(self: Self, dest: anytype) Vector4(dest) {
+            const source_info = @typeInfo(T);
+            const dest_info = @typeInfo(dest);
+
+            if (source_info == .Float and dest_info == .Int) {
+                const x = @floatToInt(dest, self.x);
+                const y = @floatToInt(dest, self.y);
+                const z = @floatToInt(dest, self.z);
+                const w = @floatToInt(dest, self.w);
+                return Vector4(dest).new(x, y, z, w);
+            }
+
+            if (source_info == .Int and dest_info == .Float) {
+                const x = @intToFloat(dest, self.x);
+                const y = @intToFloat(dest, self.y);
+                const z = @intToFloat(dest, self.z);
+                const w = @intToFloat(dest, self.w);
+                return Vector4(dest).new(x, y, z, w);
+            }
+
+            return switch (dest_info) {
+                .Float => {
+                    const x = @floatCast(dest, self.x);
+                    const y = @floatCast(dest, self.y);
+                    const z = @floatCast(dest, self.z);
+                    const w = @floatCast(dest, self.w);
+                    return Vector4(dest).new(x, y, z, w);
+                },
+                .Int => {
+                    const x = @intCast(dest, self.x);
+                    const y = @intCast(dest, self.y);
+                    const z = @intCast(dest, self.z);
+                    const w = @intCast(dest, self.w);
+                    return Vector4(dest).new(x, y, z, w);
+                },
+                else => panic(
+                    "Error, given type should be integers or float.\n",
+                    .{},
+                ),
+            };
+        }
+
+        /// Construct new vector from slice.
+        pub fn fromSlice(slice: []const T) Self {
+            return Self.new(slice[0], slice[1], slice[2], slice[3]);
+        }
+
+        /// Transform vector to array.
+        pub fn toArray(self: Self) [4]T {
+            return .{ self.x, self.y, self.z, self.w };
+        }
+
+        /// Compute the length (magnitude) of given vector |a|.
+        pub fn length(self: Self) T {
+            return @sqrt(self.dot(self));
+        }
+
+        /// Compute the distance between two points.
+        pub fn distance(a: Self, b: Self) T {
+            return length(b.sub(a));
+        }
+
+        /// Construct new normalized vector from a given vector.
+        pub fn norm(self: Self) Self {
+            var l = length(self);
+            return Self.new(
+                self.x / l,
+                self.y / l,
+                self.z / l,
+                self.w / l,
+            );
+        }
+
+        pub fn eql(left: Self, right: Self) bool {
+            return left.x == right.x and
+                left.y == right.y and
+                left.z == right.z and
+                left.w == right.w;
+        }
+
+        /// Substraction between two given vector.
+        pub fn sub(left: Self, right: Self) Self {
+            return Self.new(
+                left.x - right.x,
+                left.y - right.y,
+                left.z - right.z,
+                left.w - right.w,
+            );
+        }
+
+        /// Addition betwen two given vector.
+        pub fn add(left: Self, right: Self) Self {
+            return Self.new(
+                left.x + right.x,
+                left.y + right.y,
+                left.z + right.z,
+                left.w + right.w,
+            );
+        }
+
+        /// Multiply each components by the given scalar.
+        pub fn scale(v: Self, scalar: T) Self {
+            return Self.new(
+                v.x * scalar,
+                v.y * scalar,
+                v.z * scalar,
+                v.w * scalar,
+            );
+        }
+
+        /// Return the dot product between two given vector.
+        pub fn dot(left: Self, right: Self) T {
+            return (left.x * right.x) + (left.y * right.y) + (left.z * right.z) + (left.w * right.w);
+        }
+
+        /// Lerp between two vectors.
+        pub fn lerp(left: Self, right: Self, t: T) Self {
+            const x = root.lerp(T, left.x, right.x, t);
+            const y = root.lerp(T, left.y, right.y, t);
+            const z = root.lerp(T, left.z, right.z, t);
+            const w = root.lerp(T, left.w, right.w, t);
+            return Self.new(x, y, z, w);
+        }
+
+        /// Construct a new vector from the min components between two vectors.
+        pub fn min(left: Self, right: Self) Self {
+            return Self.new(
+                @minimum(left.x, right.x),
+                @minimum(left.y, right.y),
+                @minimum(left.z, right.z),
+                @minimum(left.w, right.w),
+            );
+        }
+
+        /// Construct a new vector from the max components between two vectors.
+        pub fn max(left: Self, right: Self) Self {
+            return Self.new(
+                @maximum(left.x, right.x),
+                @maximum(left.y, right.y),
+                @maximum(left.z, right.z),
+                @maximum(left.w, right.w),
+            );
+        }
+    };
+}
+
+test "zalgebra.Vec4.init" {
+    var _vec_0 = Vec4.new(1.5, 2.6, 3.7, 4.7);
+
+    try expectEqual(_vec_0.x, 1.5);
+    try expectEqual(_vec_0.y, 2.6);
+    try expectEqual(_vec_0.z, 3.7);
+    try expectEqual(_vec_0.w, 4.7);
+}
+
+test "zalgebra.Vec4.eql" {
+    var _vec_0 = Vec4.new(1, 2, 3, 4);
+    var _vec_1 = Vec4.new(1, 2, 3, 4);
+    var _vec_2 = Vec4.new(1, 2, 3, 5);
+    try expectEqual(Vec4.eql(_vec_0, _vec_1), true);
+    try expectEqual(Vec4.eql(_vec_0, _vec_2), false);
+}
+
+test "zalgebra.Vec4.set" {
+    var _vec_0 = Vec4.new(2.5, 2.5, 2.5, 2.5);
+    var _vec_1 = Vec4.set(2.5);
+    try expectEqual(Vec4.eql(_vec_0, _vec_1), true);
+}
+
+test "zalgebra.Vec4.negate" {
+    var a = Vec4.set(5);
+    var b = Vec4.set(-5);
+    try expectEqual(Vec4.eql(a.negate(), b), true);
+}
+
+test "zalgebra.Vec4.toArray" {
+    const _vec_0 = Vec4.new(0, 1, 0, 1).toArray();
+    const _vec_1 = [_]f32{ 0, 1, 0, 1 };
+
+    try expectEqual(std.mem.eql(f32, &_vec_0, &_vec_1), true);
+}
+
+test "zalgebra.Vec4.length" {
+    var _vec_0 = Vec4.new(1.5, 2.6, 3.7, 4.7);
+    try expectEqual(_vec_0.length(), 6.69253301);
+}
+
+test "zalgebra.Vec4.distance" {
+    var a = Vec4.new(0, 0, 0, 0);
+    var b = Vec4.new(-1, 0, 0, 0);
+    var c = Vec4.new(0, 5, 0, 0);
+
+    try expectEqual(Vec4.distance(a, b), 1);
+    try expectEqual(Vec4.distance(a, c), 5);
+}
+
+test "zalgebra.Vec4.normalize" {
+    var _vec_0 = Vec4.new(1.5, 2.6, 3.7, 4.0);
+    try expectEqual(Vec4.eql(
+        _vec_0.norm(),
+        Vec4.new(0.241121411, 0.417943745, 0.594766139, 0.642990410),
+    ), true);
+}
+
+test "zalgebra.Vec4.sub" {
+    var _vec_0 = Vec4.new(1, 2, 3, 6);
+    var _vec_1 = Vec4.new(2, 2, 3, 5);
+    try expectEqual(Vec4.eql(
+        Vec4.sub(_vec_0, _vec_1),
+        Vec4.new(-1, 0, 0, 1),
+    ), true);
+}
+
+test "zalgebra.Vec4.add" {
+    var _vec_0 = Vec4.new(1, 2, 3, 5);
+    var _vec_1 = Vec4.new(2, 2, 3, 6);
+    try expectEqual(Vec4.eql(
+        Vec4.add(_vec_0, _vec_1),
+        Vec4.new(3, 4, 6, 11),
+    ), true);
+}
+
+test "zalgebra.Vec4.scale" {
+    var _vec_0 = Vec4.new(1, 2, 3, 4);
+    try expectEqual(Vec4.eql(
+        Vec4.scale(_vec_0, 5),
+        Vec4.new(5, 10, 15, 20),
+    ), true);
+}
+
+test "zalgebra.Vec4.dot" {
+    var _vec_0 = Vec4.new(1.5, 2.6, 3.7, 5);
+    var _vec_1 = Vec4.new(2.5, 3.45, 1.0, 1);
+
+    try expectEqual(Vec4.dot(_vec_0, _vec_1), 21.4200000);
+}
+
+test "zalgebra.Vec4.lerp" {
+    var _vec_0 = Vec4.new(-10.0, 0.0, -10.0, -10.0);
+    var _vec_1 = Vec4.new(10.0, 10.0, 10.0, 10.0);
+
+    try expectEqual(Vec4.eql(
+        Vec4.lerp(_vec_0, _vec_1, 0.5),
+        Vec4.new(0.0, 5.0, 0.0, 0.0),
+    ), true);
+}
+
+test "zalgebra.Vec4.min" {
+    var _vec_0 = Vec4.new(10.0, -2.0, 0.0, 1.0);
+    var _vec_1 = Vec4.new(-10.0, 5.0, 0.0, 1.01);
+
+    try expectEqual(Vec4.eql(
+        Vec4.min(_vec_0, _vec_1),
+        Vec4.new(-10.0, -2.0, 0.0, 1.0),
+    ), true);
+}
+
+test "zalgebra.Vec4.max" {
+    var _vec_0 = Vec4.new(10.0, -2.0, 0.0, 1.0);
+    var _vec_1 = Vec4.new(-10.0, 5.0, 0.0, 1.01);
+
+    try expectEqual(Vec4.eql(
+        Vec4.max(_vec_0, _vec_1),
+        Vec4.new(10.0, 5.0, 0.0, 1.01),
+    ), true);
+}
+
+test "zalgebra.Vec4.fromSlice" {
+    const array = [4]f32{ 2, 4, 3, 6 };
+    try expectEqual(Vec4.eql(
+        Vec4.fromSlice(&array),
+        Vec4.new(2, 4, 3, 6),
+    ), true);
+}
+
+test "zalgebra.Vec4.cast" {
+    const a = Vec4_i32.new(3, 6, 2, 0);
+    const b = Vec4_usize.new(3, 6, 2, 0);
+
+    try expectEqual(Vec4_usize.eql(a.cast(usize), b), true);
+
+    const c = Vec4.new(3.5, 6.5, 2.0, 0);
+    const d = Vec4_f64.new(3.5, 6.5, 2, 0.0);
+
+    try expectEqual(Vec4_f64.eql(c.cast(f64), d), true);
+
+    const e = Vec4_i32.new(3, 6, 2, 0);
+    const f = Vec4.new(3.0, 6.0, 2.0, 0.0);
+
+    try expectEqual(Vec4.eql(e.cast(f32), f), true);
+
+    const g = Vec4.new(3.0, 6.0, 2.0, 0.0);
+    const h = Vec4_i32.new(3, 6, 2, 0);
+
+    try expectEqual(Vec4_i32.eql(g.cast(i32), h), true);
+}
