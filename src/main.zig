eabb9fd992f3cc17039be0312f563c4ac48339a4
Caleb Gingles <git@nairou.com>
2024-01-12 17:58:44 -0500

    Add 3x3 matrix

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const expectEqual = std.testing.expectEqual;
 const math = std.math;
 
 pub usingnamespace @import("generic_vector.zig");
+pub usingnamespace @import("mat3.zig");
 pub usingnamespace @import("mat4.zig");
 pub usingnamespace @import("quaternion.zig");
 
